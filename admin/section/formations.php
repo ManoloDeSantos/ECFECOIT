cb260c7f511ec3bb6fda69335359f35058d0ded3
Manolo <lolosub@gmail.com>
2022-04-01 15:19:11 +0200

    interface table enregistrements

--- admin/section/formations.php
+++ admin/section/formations.php
@@ -7,35 +7,45 @@
     </div>
     <div class="card-body">
       <form method="post" enctype="multipart/form-data">
-
-      <div class = "form-group">
-      <label for="txtID">ID:</label>
-      <input type="text" class="form-control" name="txtID" id="txtid" placeholder="ID">
-      </div>
-      <div class = "form-group">
-      <label for="txtNombre">Formation:</label>
-      <input type="text" class="form-control" name="txtNom" id="txNom" placeholder="Formation">
-      </div>
-      <div class = "form-group">
-      <label for="txtNombre">Image:</label>
-      <input type="file" class="form-control" name="txtNom" id="txNom" placeholder="Formation">
-      </div>
-
-    <div class="btn-group" role="group" aria-label="">
-      <button type="button" class="btn btn-success">Ajouter</button>
-      <button type="button" class="btn btn-warning">Modifier</button>
-      <button type="button" class="btn btn-info">Annuler</button>
+        <div class = "form-group">
+        <label for="txtID">ID:</label>
+        <input type="text" class="form-control" name="txtID" id="txtid" placeholder="ID">
+        </div>
+        <div class = "form-group">
+        <label for="txtNombre">Formation:</label>
+        <input type="text" class="form-control" name="txtNom" id="txNom" placeholder="Formation">
+        </div>
+        <div class = "form-group">
+        <label for="txtNombre">Image:</label>
+        <input type="file" class="form-control" name="txtNom" id="txNom" placeholder="Formation">
+        </div>
+        <div class="btn-group" role="group" aria-label="">
+          <button type="button" class="btn btn-success">Ajouter</button>
+          <button type="button" class="btn btn-warning">Modifier</button>
+          <button type="button" class="btn btn-info">Annuler</button>
+        </div>
+      </form>
     </div>
-  
-  </form>
-
-    </div>
-
   </div>
-
-
-  
-  
 </div>
-  tabla mostrar libros
+<div class="col-md-7">
+  <table class="table table-bordered">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Formation</th>
+        <th>Image</th>
+        <th>Actions</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>2</td>
+        <td>apprendre PHP</td>
+        <td>image.jpg</td>
+        <td>sélectionner | effacer</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
 <?php include("../template/footer.php")?>  
