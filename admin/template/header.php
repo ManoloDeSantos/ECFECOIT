1fa90e5d3916717e54feb7660b91460c58acb188
Manolo <lolosub@gmail.com>
2022-04-01 13:22:42 +0200

    ajouter des section fermer et formations

--- admin/template/header.php
+++ admin/template/header.php
@@ -7,13 +7,14 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
   <body>
+    <?php $url="http://".$_SERVER['HTTP_HOST']."/ECFECOIT"?>
     <nav class="navbar navbar-expand navbar-light bg-light">
         <div class="nav navbar-nav">
             <a class="nav-item nav-link active" href="#">Administrateur du site Web<span class="sr-only">(Current)</span></a>
-            <a class="nav-item nav-link" href="#">Accueil</a>
-            <a class="nav-item nav-link" href="#">Formations</a>
-            <a class="nav-item nav-link" href="#">Fermer Sesion</a>
-            <a class="nav-item nav-link" href="#">Voir site web</a>
+            <a class="nav-item nav-link" href="<?php echo $url;?>/admin/accueil.php">Accueil</a>
+            <a class="nav-item nav-link" href="<?php echo $url;?>/admin/section/formations.php">Formations</a>
+            <a class="nav-item nav-link" href="<?php echo $url;?>/admin/section/fermer.php">Fermer Sesion</a>
+            <a class="nav-item nav-link" href="<?php echo $url;?>">Voir site web</a>
         </div>
     </nav>
     <div class="container">
